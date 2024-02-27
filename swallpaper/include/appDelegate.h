7fc7a221f27b0ca50968ab26a8b6454d35f879d6
ViperTools <cboatyt@gmail.com>
2024-02-27 12:38:53 -0600

    Heavily optimize menu bar customization

---
+++ swallpaper/include/appDelegate.h
@@ -0,0 +1,12 @@
+//
+//  appDelegate.h
+//  swallpaper
+//
+//  Created by Caleb Boatcallie on 2/27/24.
+//
+
+#ifndef appDelegate_h
+#define appDelegate_h
+
+
+#endif /* appDelegate_h */
---
+++ swallpaper/src/appDelegate.m
@@ -0,0 +1,8 @@
+//
+//  appDelegate.m
+//  swallpaper
+//
+//  Created by Caleb Boatcallie on 2/27/24.
+//
+
+#import <Foundation/Foundation.h>
