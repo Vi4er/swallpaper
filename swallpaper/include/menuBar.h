7cf5a3b5ea58cc6c6203c6c5bd27511c206cac24
ViperTools <cboatyt@gmail.com>
2024-02-26 19:55:28 -0600

    Started work on custom menu bar

---
+++ swallpaper/include/menuBar.h
@@ -0,0 +1,12 @@
+//
+//  menuBar.h
+//  swallpaper
+//
+//  Created by Caleb Boatcallie on 2/26/24.
+//
+
+#ifndef menuBar_h
+#define menuBar_h
+
+
+#endif /* menuBar_h */
---
+++ swallpaper/src/menuBar.m
@@ -0,0 +1,8 @@
+//
+//  menuBar.m
+//  swallpaper
+//
+//  Created by Caleb Boatcallie on 2/26/24.
+//
+
+#import <Foundation/Foundation.h>
